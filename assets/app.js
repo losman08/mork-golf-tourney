e8d198a68853337ccaeca002138cdb1fcd7a8068
losman08 <losman08@yahoo.com>
2026-06-10 13:14:47 -0700

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,11 @@
-console.log("✅ app.js is connected and running");
+console.log("STEP 5: starting data load");
+
+fetch("data/players.json")
+  .then(response => response.json())
+  .then(data => {
+    console.log("PLAYERS DATA LOADED:");
+    console.log(data);
+  })
+  .catch(error => {
+    console.error("FAILED TO LOAD DATA:", error);
+  });
